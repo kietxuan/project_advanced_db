5585c0cbe7bdf80fe02b0a82d3e404be091f12d4
kietxuan <kietnguyenxuan2401@gmail.com>
2026-04-22 23:06:47 +0700

    initial commit

--- app/main.py
+++ app/main.py
@@ -19,7 +19,12 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=[
+        "http://127.0.0.1:5500", 
+        "http://localhost:5500",
+        "https://misinfo-frontend.vercel.app", 
+        "*" 
+    ], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
